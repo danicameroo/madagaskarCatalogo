76cd7c0e8dc90b65f7dd2b4877b865251c9dd894
inversionesMadagagaskar <inversionesmadagaskar@gmail.com>
2024-04-11 12:40:09 -0400

    vercel config

--- server.js
+++ server.js
@@ -1,34 +1,82 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const productsRoute = require("./routes/products");
-const cors = require("cors");
+const dotenv = require("dotenv")
+const cors = require('cors');
+app.use(cors());
 
 dotenv.config();
-
-// Configuración de CORS para permitir acceso desde tu página web
-app.use(cors({
-  origin: "https://danicameroo.github.io/CatalogoMadagaskar/" // Reemplaza con la URL de tu página web
-}));
-
-// Conexión a la base de datos
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("DB connection successful!"))
-  .catch((err) => console.error("DB connection error:", err));
+    .connect(process.env.MONGO_URL)
+    .then(()=>console.log("DBconnnection succesfull!"))
+    .catch((err)=>{
+        console.log(err);
+    });
 
-// Middleware para el manejo de datos JSON
-app.use(express.json());
+//CREATE
+app.post("/api/products/" ,async (req,res)=>{
+  const newProduct = new Product(req.body)
 
-// Rutas
-app.use("/api/products", productsRoute);
+  try{
+      const savedProduct = await newProduct.save();
+      res.status(200).json(savedProduct)
+  }catch(err){
+      res.status(500).json(err)
+  }
+})
 
-// Iniciar el servidor
-const port = process.env.PORT || 3030;
-app.listen(port, () => {
-  console.log(`Backend server is running on port ${port}`);
+//UPDATE
+app.put("/api/products/:id", async (req,res) =>{  
+  try{
+      const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
+          $set: req.body
+      },{new:true});
+      res.status(200).json(updatedProduct);
+  }catch(err){
+      res.status(500).json(err)
+  }
+});
+
+//DELETE
+app.delete("/api/products/:id", async (req,res)=>{
+  try{
+      await Product.findByIdAndDelete(req.params.id);
+      res.status(200).json("Product has been deleted");
+  }catch(err){ // Se agrega el parámetro 'err' en el bloque catch
+      res.status(500).json(err);
+  }
+});
+
+//GET PRODUCT
+app.get("/api/products/find/:id", async (req,res)=>{
+  try{
+      const product = await Product.findById(req.params.id);
+      res.status(200).json(product);
+  }catch(err){
+      res.status(500).json(err);
+  }
+});
+
+
+//GET ALL PRODUCTS
+app.get("/api/products/", async (req,res)=>{
+  const qNew = req.query.new;
+  try{
+      let products;
+      if(qNew){
+          products = await Product.find().sort({createdAt: -1}).limit(5)
+      }else if(qCategory){
+          products = await Product.find({categories:{$in: [qCategory],}})
+      }else{
+          products = await Product.find()
+      }
+      res.status(200).json(products);
+  }catch(err){
+      res.status(500).json(err);
+  }
+});
+
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Backend server is running!");
 });
